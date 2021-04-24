fb98454c53cf463e826c107ccc272b6b9492532c
robonexx <robertwagar@gmail.com>
2021-04-24 19:05:55 +0200

    on to the next

---
+++ controllers/productController.js
@@ -0,0 +1,70 @@
+const Product = require('../models/productModel')
+const { getPostData } = require('../utils')
+
+// desc Gets all products
+// route GEt api/products
+async function getProducts(req, res) {
+    try{
+
+        const products = await Product.findAll()
+
+        res.writeHead(200, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify(products))
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+// desc Gets a single product
+// route GEt api/products/:id
+
+async function getProduct(req, res, id) {
+    try{
+        const product = await Product.findById(id)
+
+        if(!product) {
+        res.writeHead(404, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify({message: 'Product not found'}))
+    } else {
+        res.writeHead(200, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify(product))
+    }
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+
+// cretae new product 
+// post request
+async function createProduct(req, res) {
+    try{
+
+        // testar ny product
+
+        const body = await getPostData(req)
+
+        const { title, description, price } = JSON.parse(body)
+
+        const product = {
+            title,
+            description,
+            price
+        }
+
+        const newProduct = await Product.create(product)
+
+        res.writeHead(201, {'Content-Type': 'application/json'})
+        return res.end(JSON.stringify(newProduct))
+        
+
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+module.exports = {
+    getProducts,
+    getProduct,
+    createProduct
+}
---
+++ server.js
@@ -0,0 +1,55 @@
+// fullfjärdad node.js server rest-api från scracth så man förstår vad som händer
+// Började med denna https://www.youtube.com/watch?v=_1xa8Bsho6A&t=1s Traversy Media på Youtube. 
+// Köpte denna kurs på UDEMY https://www.udemy.com/share/101WGiCUMecVdbQHQ=/ Som jag börjat gå igenom
+
+//utan express
+
+const http = require('http')
+const {getProducts, getProduct, createProduct } = require('./controllers/productController')
+
+//startar upp server/ skapar en server 
+
+const server = http.createServer((req, res) => {
+
+    // som innehåller text fil i form av html, hello world h1 tag
+    /* res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.write(`<h1>Hello World </h1>`)
+    res.end() */
+
+    if(req.url === '/api/products' && req.method === 'GET') {
+        getProducts(req, res)
+
+    } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET')
+    {
+        const id = req.url.split('/') [3]
+        getProduct(req, res, id)
+        /* res.writeHead(200, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify(products)) */
+    } else if (req.url === '/api/products' && req.method === 'POST'){
+            createProduct(req, res)
+    } else {
+        res.writeHead(404, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify({message: 'Route not found'}))
+    }
+})
+
+// skapar port som servern lyssnar på i detta fall port 5000
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+
+// testar själva att göra en api och skicka info
+
+// const streetstyles = require('./data/streetstyles')
+
+    /* if(req.url === '/api/streetstyles' && req.method === 'GET') {
+        res.writeHead(200, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify(streetstyles))
+    } else {
+        res.writeHead(404, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify({message: 'Route not found'}))
+    }
+ */
